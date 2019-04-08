c930320f5f8fb1d70b2d11183d54437423652566
Dennis <dang.dennis21@gmail.com>
2019-04-08 01:20:50 -0700

    added data for OAK, SJC and LAX

--- data/dataFilter.py
+++ data/dataFilter.py
@@ -3,45 +3,74 @@ import csv
 fieldnames = ['CARRIER_NAME', 'ORIGIN', 'ORIGIN_CITY_NAME', 'ORIGIN_COUNTRY_NAME', 'DEST',
 'DEST_CITY_NAME', 'DEST_COUNTRY_NAME', 'PASSENGER', 'INSTANCES']
 
-# Copy row if origin airport matches parameter airport
+outboundFlights = dict()
+inboundFlights = dict()
+
 def filter(fileName, city, year):
+    outputFileNames = ['filtered/' + city + '_' + year + '-inbound.csv', 'filtered/' + city + '_' + year + '-outbound.csv']
     with open(fileName, mode='r') as inputFile:
         inputData = csv.DictReader(inputFile)
-        
-        # write to output for inbound and outbound flights in separate files
-        outputFileNames = ['filtered/' + city + '_' + year + '-inbound.csv', 'filtered/' + city + '_' + year + '-outbound.csv']
-        with open(outputFileNames[0], mode='wb') as outputFileInbound:      # open inbound flights file
-            with open(outputFileNames[1], mode='wb') as outputFileOutbound: # open outbound flights file
-                fileWriter_inbound = csv.DictWriter(outputFileInbound, fieldnames)
-                fileWriter_outbound = csv.DictWriter(outputFileOutbound, fieldnames)
-                fileWriter_inbound.writeheader()
-                fileWriter_outbound.writeheader()
-                for row in inputData:
-                    if row['ORIGIN'] == city:
-                        writeToFile(fileWriter_outbound, row)
-                    elif row['DEST'] == city:
-                        writeToFile(fileWriter_inbound, row)
+        for row in inputData:
+            isPassengerFlight = True if row['AIRCRAFT_CONFIG'] == '1' else False
+            if row['ORIGIN'] == city: # outbound flights
+                if row['DEST'] != city:
+                    if row['DEST'] not in outboundFlights:
+                        addToDict(row['DEST'], outboundFlights, row, isPassengerFlight)
+                    else:
+                        outboundFlights[row['DEST']]['INSTANCES'] += 1
+            elif row['DEST'] == city: # inbound flights
+                if row['ORIGIN'] != city:
+                    if row['ORIGIN'] not in inboundFlights:
+                        addToDict(row['ORIGIN'], inboundFlights, row, isPassengerFlight)
+                    else:
+                        inboundFlights[row['ORIGIN']]['INSTANCES'] += 1
+    
+    writeToFile(outputFileNames[0], inboundFlights)
+    writeToFile(outputFileNames[1], outboundFlights)
 
 
-def writeToFile(fileHandler, row):
-    passenger = True if row['AIRCRAFT_CONFIG'] == '1' else False
-    fileHandler.writerow({fieldnames[0] : row[fieldnames[0]],
-    fieldnames[1] : row[fieldnames[1]],
-    fieldnames[2] : row[fieldnames[2]],
-    fieldnames[3] : row[fieldnames[3]],
-    fieldnames[4] : row[fieldnames[4]],
-    fieldnames[5] : row[fieldnames[5]],
-    fieldnames[6] : row[fieldnames[6]],
-    fieldnames[7] : str(passenger)})
+def addToDict(key, dictHandler, row, isPassengerFlight):
+    dictHandler[key] = { 
+        fieldnames[0] : row[fieldnames[0]],
+        fieldnames[1] : row[fieldnames[1]],
+        fieldnames[2] : row[fieldnames[2]],
+        fieldnames[3] : row[fieldnames[3]],
+        fieldnames[4] : row[fieldnames[4]],
+        fieldnames[5] : row[fieldnames[5]],
+        fieldnames[6] : row[fieldnames[6]],
+        fieldnames[7] : str(isPassengerFlight),
+        fieldnames[8] : 1,
+    }
+
+def writeToFile(fileName, dictHandler):
+    with open(fileName, mode='wb') as file:
+        fileWriter = csv.DictWriter(file, fieldnames)
+        fileWriter.writeheader()
+        for key, value in dictHandler.items():
+            fileWriter.writerow({
+                fieldnames[0] : value[fieldnames[0]],
+                fieldnames[1] : value[fieldnames[1]],
+                fieldnames[2] : value[fieldnames[2]],
+                fieldnames[3] : value[fieldnames[3]],
+                fieldnames[4] : value[fieldnames[4]],
+                fieldnames[5] : value[fieldnames[5]],
+                fieldnames[6] : value[fieldnames[6]],
+                fieldnames[7] : value[fieldnames[7]],
+                fieldnames[8] : value[fieldnames[8]],
+            })
+
+
 
 if __name__ == '__main__':
     # filter for year 2017
-    filter('raw/2017-all.csv', 'SFO', '2017')
-    filter('raw/2017-all.csv', 'SJC', '2017')
-    filter('raw/2017-all.csv', 'OAK', '2017')
-    print('2017 done')
-    # filter for year 2018
-    filter('raw/2018-all.csv', 'SFO', '2018')
-    filter('raw/2018-all.csv', 'SJC', '2018')
-    filter('raw/2018-all.csv', 'OAK', '2018')
+    #filter('raw/2017-all.csv', 'SFO', '2017')
+    #filter('raw/2017-all.csv', 'SJC', '2017')
+    #filter('raw/2017-all.csv', 'OAK', '2017')
+    #filter('raw/2017-all.csv', 'LAX', '2017')
+    # print('2017 done')
+    # # filter for year 2018
+    # filter('raw/2018-all.csv', 'SFO', '2018')
+    # filter('raw/2018-all.csv', 'SJC', '2018')
+    # filter('raw/2018-all.csv', 'OAK', '2018')
+    # filter('raw/2018-all.csv', 'LAX', '2018')
     print('2018 done')
--- js/index.js
+++ js/index.js
@@ -1,46 +1,115 @@
-// mapbox api key
-var key = 'pk.eyJ1IjoiZGRhbmczIiwiYSI6ImNqdHMwaThjdzB2dTA0OHBjaHVveHc3ZXAifQ.JcbAc7CDY40asCSq6olQgw'
-
-// Options for map
-const options = {
-  lat: 20.407,
-  lng: 10.287,
-  zoom: 3,
-  style: 'mapbox://styles/ddang3/cjtsayqjq7rcf1fmttz9l8oy5',
-  pitch: 0,
-  studio: true,
-};
-
-const mappa = new Mappa('Mapbox', key);
+const mappa = new Mappa('Leaflet');
 let myMap;
 let canvas;
 let airportData = [];
-let airportLocation;
+let airportLocation = [];
+let positions = [];
+let maxCount = 0;
+let focusCityData;
+let focusCityName = 'LAX';
+
+let options = {
+  lat: 35.6213,
+  lng: -120.3790,
+  zoom: 7,
+  style: "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+}
 
 function preload() {
-  airportData.push(loadTable('../data/filtered/SFO_2017-outbound.csv', 'csv', 'header'));
+  airportData.push({
+      'SFO' : { 'outbound' : loadTable('../data/filtered/SFO_2017-outbound.csv', 'csv', 'header'),
+                'inbound' : loadTable('../data/filtered/SFO_2017-inbound.csv', 'csv', 'header')},
+      'OAK' : { 'outbound' : loadTable('../data/filtered/OAK_2017-outbound.csv', 'csv', 'header'),
+                'inbound' : loadTable('../data/filtered/OAK_2017-inbound.csv', 'csv', 'header')},
+      'SJC' : { 'outbound' : loadTable('../data/filtered/SJC_2017-outbound.csv', 'csv', 'header'),
+                'inbound' : loadTable('../data/filtered/SJC_2017-inbound.csv', 'csv', 'header')},
+      'LAX' : { 'outbound' : loadTable('../data/filtered/LAX_2017-outbound.csv', 'csv', 'header'),
+                'inbound' : loadTable('../data/filtered/LAX_2017-inbound.csv', 'csv', 'header')},
+    });
+
   airportLocation = loadJSON('../data/airports.json');
 }
 
 function setup() {
-    // Create a tile map and overlay the canvas on top.
+  // Create a tile map and overlay the canvas on top.
   canvas = createCanvas(window.innerWidth, window.innerHeight);
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
-
-  // for debugging
-} 
+  updateLocations();
+  myMap.onChange(updateLocations);
+}
 
 function draw() {
-    clear();
-    for(let row of airportData[0].rows) {
-       let city = row.get('DEST');
-       console.log(city);
-      // let cityLocation = airportLocation[city];
-      // let lat = cityLocation['latitude'];
-      // let long = cityLocation['longitude'];
-      // const pos = myMap.latLngToPixel(lat, long);
-      //ellipse(200, 500, 5, 5);
-    }
-    noLoop();
+  clear();
+
+  // draw rotating star around focus city
+  fill(255,255,0, 100);
+  push();
+  translate(focusCityData.xPos, focusCityData.yPos);
+  rotate(frameCount / 200.0);
+  star(0, 0, 15, 35, 5);
+  pop();
+
+  // draw cities in red
+  fill(255,0,0,100);  
+  for(let p of positions) {
+    ellipse(p.xPos, p.yPos, p.diameter*myMap.zoom());
+  }
+}
+
+
+function updateLocations() {
+  positions = [];
+  maxCount = 0;
+  for (let row of airportData[0][focusCityName]['outbound'].rows) { 
+    let city = row.get('DEST');
+    let cityLocation = airportLocation[city];
+    let lat = cityLocation['latitude'];
+    let long = cityLocation['longitude'];
+    let instances = parseInt(row.get('INSTANCES'));
+    let pos = myMap.latLngToPixel(lat, long);
+    if(instances > maxCount) maxCount = instances;
+    positions.push( {
+      xPos: pos.x,
+      yPos: pos.y,
+      count: instances,
+    });
+  }
+  let maxDiameter = sqrt(maxCount);
+
+  for(let pos of positions) {
+    pos.diameter = map(sqrt(pos.count), 1, maxDiameter, 2,15);
+  }
+  updateFocusCity(focusCityName);
+}
+
+function updateFocusCity(name) {
+  let location = airportLocation[name];
+  let lat = location['latitude'];
+  let long = location['longitude'];
+  let pos = myMap.latLngToPixel(lat, long);
+  focusCityData = {
+    xPos: pos.x,
+    yPos: pos.y,
+  };
+}
+
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+function setFocusAirport(name) {
+  focusCityName = name;
+  updateLocations();
 }
